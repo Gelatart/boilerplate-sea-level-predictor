6de9ca4c67e51cdff7543fb473467451bbefe2fb
Gelatart <daniel.f.wood@gmail.com>
2025-11-21 21:53:03 -0700

    Some fixes to first line of best fit

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,16 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
 
 def draw_plot():
     # Read data from file
     df = pd.read_csv("epa-sea-level.csv")
 
     # Create scatter plot
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
 
+    plt.scatter(x,y)
 
     # Create first line of best fit
+    slope, intercept, r, p, stderr = linregress(x, y)
 
+    x_extend = np.linspace(min(x) - 10, 2050, 100)
+    y_pred = slope*x_extend + intercept
+    plt.plot(x_extend, y_pred)
 
     # Create second line of best fit
 
